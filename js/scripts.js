2d309f357c01280b648baffcd2d57437c27e573c
Jerrod Styrk <jstyrk@citadel.edu>
2020-06-05 11:53:23 -0700

    Add let for all inputs into javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,27 @@
 $(document).ready(function() {
   $("#questions").submit(function(event) {
     const nameInput = $("input#name").val();
+    let oS = $("input:radio[name=oS]:checked").val();
+    let budget = $("input:radio[name=budget]:checked").val();
+    let workStyle = $("input:radio[name=workStyle]:checked").val();
+    let develop = $("input:radio[name=develop]:checked").val();
+    let unwind = $("input:radio[name=unwind]:checked").val();
+    
+    // $("#cSharp").hide();
+    // $("#ruby").hide();
+    // $("#python").hide();
+    // $("#javaScript").hide();
+    // $("#swift").hide();
 
+    if(oS === "windows" ) {
+      $("#cSharp").show;
+    }
+    
+    
+    
     $(".name").text(nameInput);
     $("#choice").show();
-
+    
     event.preventDefault();
   });
 });
