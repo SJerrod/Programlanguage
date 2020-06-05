f8e6f7fe61bb2719e3bae5a828fcc00605d3a2b8
Jerrod Styrk <jstyrk@citadel.edu>
2020-06-05 14:39:57 -0700

    Add hide feature to questions and reset button to answers

--- js/scripts.js
+++ js/scripts.js
@@ -22,14 +22,59 @@ $(document).ready(function() {
     if(oS === "windows" && workStyle === "easy" && develop === "gameDev") {
       $("#cSharp").show();
     } else if (oS === "windows" && workStyle === "easy" && develop === "data") {
+      $("#ruby").show();
+    }
+
+    if(oS === "windows" && workStyle === "hard" && develop === "webDev") {
+      $("#javaScript").show();
+    } else if (oS === "windows" && workStyle === "hard" && develop === "data") {
+      $("#javaScript").show();
+    }
+
+    if(oS === "windows" && workStyle === "hard" && develop === "gameDev") {
       $("#cSharp").show();
+    } else if (oS === "windows" && workStyle === "hard" && develop === "software") {
+      $("#cSharp").show();
+    }
+
+    if(oS === "apple" && workStyle === "easy" && develop === "webDev") {
+      $("#ruby").show();
+    } else if (oS === "apple" && workStyle === "easy" && develop === "data") {
+      $("#ruby").show();
+    }
+
+    if(oS === "apple" && workStyle === "easy" && develop === "gameDev") {
+      $("#python").show();
+    } else if (oS === "apple" && workStyle === "easy" && develop === "software") {
+      $("#python").show();
+    }
+
+    if(oS === "apple" && workStyle === "hard" && develop === "webDev") {
+      $("#javaScript").show();
+    } else if (oS === "apple" && workStyle === "hard" && develop === "software") {
+      $("#swift").show();
+    }
+
+    if(oS === "apple" && workStyle === "hard" && develop === "gameDev") {
+      $("#swift").show();
+    } else if (oS === "apple" && workStyle === "hard" && develop === "software") {
+      $("#swift").show();
     }
     
     
     
     $(".name").text(nameInput);
     $("#choice").show();
-    
+    $("#all").hide();
+    $(".btn").click(function(){
+      $("#all").show();
+      $("#cSharp").hide();
+      $("#ruby").hide();
+      $("#python").hide();
+      $("#javaScript").hide();
+      $("#swift").hide();
+    });
+
     event.preventDefault();
   });
 });
